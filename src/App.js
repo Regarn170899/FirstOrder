7f19e741f8eb2d1d4758e31e3f9efbe80336c753
Regarn170899 <1526497@gmail.com>
2023-08-05 23:51:23 +0300

    add first change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./Components/Header/Header";
+import FirstSector from "./Components/FirstSector/FirstSector";
+import SectorTwo from "./Components/SectorTwo/SectorTwo";
+import SectorThree from "./Components/SectorThree/SectorThree";
+import AnimationLine from "./Components/AnimationLine/AnimationLine";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={'container'}>
+      <Header/>
+        <FirstSector/>
+        <SectorTwo/>
+        <SectorThree/>
+        <AnimationLine/>
     </div>
   );
 }
---
+++ src/Components/SectorTwo/SectorTwo.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from "./SectorTwo.module.css"
+
+const SectorTwo = () => {
+    return (
+        <div className={styles.sectorContainer}>
+            <div className={styles.topContainer}>
+                <div className={styles.imgContainer}>
+                </div>
+                <div className={styles.textContainer}>
+                    <h4 className={styles.headerText}>О нас</h4>
+                    <p>Добро пожаловать на наш сайт! Каждый родитель мечтает видеть своего ребенка счастливым! Мы создали наш ,, Кабинет дошкольного развития,, в первую очередь для того, чтобы приходя к нам Ваши дети чувствовали себя самыми талантливыми,самыми разумными, самыми лучшими!!! Нам есть чем гордиться - большинство наших клиентов пришли к нам по рекомендации друзей и родителей наших учеников!</p>
+                </div>
+            </div>
+            <div>
+                <div className={styles.whiteBlock}></div>
+            </div>
+        </div>
+    );
+};
+
+export default SectorTwo;
