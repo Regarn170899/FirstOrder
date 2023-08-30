5be90a2009274ec4bd142a16daea2e8673fdc2f7
Regarn170899 <1526497@gmail.com>
2023-08-30 21:25:40 +0300

    add

--- src/App.js
+++ src/App.js
@@ -4,7 +4,24 @@ import FirstSector from "./Components/FirstSector/FirstSector";
 import SectorTwo from "./Components/SectorTwo/SectorTwo";
 import SectorThree from "./Components/SectorThree/SectorThree";
 import AnimationLine from "./Components/AnimationLine/AnimationLine";
+import OurTeachers from "./Components/OurTeachers/OurTeachers";
+import Gallery from "react-photo-gallery";
+import item1 from "./Rectangle 21.jpg";
+import item2 from "./assets/img/item2.svg";
+import item3 from "./assets/img/item3.svg";
+import item4 from "./assets/img/item4.svg";
+import item5 from "./assets/img/item5.svg";
+import item6 from "./assets/img/item6.svg";
+import item7 from "./assets/img/item7.svg";
+import item8 from "./assets/img/item8.svg";
 
+const Photo=[
+      {
+        src: 'https://socket.dev/images/logo-280x80.png',
+        width: 10,
+        height: 1
+      },
+];
 function App() {
   return (
     <div className={'container'}>
@@ -13,6 +30,8 @@ function App() {
         <SectorTwo/>
         <SectorThree/>
         <AnimationLine/>
+        <OurTeachers/>
+      <Gallery photos={Photo}/>
     </div>
   );
 }
